ec15e14ef59a5f8247129bcf11d91d25f1da6c74
이선우 <lsw96@naver.com>
2021-09-09 12:30:21 +0000

    ADD

---
+++ index.js
@@ -0,0 +1,23 @@
+const textarea = document.getElementById('text');
+const changeWord = document.getElementById('changed-word');
+const targetWord  = document.getElementById('target-word');
+const submit = document.getElementById('submit');
+const result = document.getElementById('new-text');
+
+
+// 함수를 만드는 두가지 방법(function)
+function changeText(text, word, newWord) {
+	const changedText = text.replaceAll(word, newWord)
+	return changedText
+}
+
+// 함수를 만드는 두가지 방법(arrow function)
+const onClickSubmit = (e) => {
+	const text = textarea.value
+	const word = targetWord.value
+	const newWord = changeWord.value
+	
+	result.value = changeText(text, word, newWord)
+}
+
+submit.addEventListener('click', onClickSubmit)
