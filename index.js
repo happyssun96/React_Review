37a9c97715dd6fe60de23d13c97be8e40380169e
이선우 <lsw96@naver.com>
2021-09-12 04:02:45 +0000

    [FIX] CSS

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const targetWord  = document.getElementById('target-word');
 const submit = document.getElementById('submit');
 const result = document.getElementById('new-text');
 
-
 // 함수를 만드는 두가지 방법(function)
 function changeText(text, word, newWord) {
 	const changedText = text.replaceAll(word, newWord)
